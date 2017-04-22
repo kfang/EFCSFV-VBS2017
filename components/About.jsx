029419cb9b29ca76919281b6f4e5d99f5d9e702f
hkr.fang <hkr.fang@gmail.com>
2017-04-22 11:24:24 -0700

    updated homepage with more information; working on schedule. wip

--- components/About.jsx
+++ components/About.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 
+import Paper from 'material-ui/Paper'
+import {Card, CardActions, CardHeader, CardMedia, CardText, CardTitle} from 'material-ui/Card'
+import {Tabs, Tab} from 'material-ui/Tabs'
+import FlatButton from 'material-ui/FlatButton'
+
+import styles from '../css/about.css'
+
 class About extends React.Component {
     render(){
-        return <div>
-            <h1>About Page</h1>
-        </div>
+        return <Tabs className={styles.container}>
+            <Tab label="About Us">
+                <Paper className={styles.aboutUs}>
+                    <Card>
+                        <CardHeader title="Who We Are" subtitle="Harvest San Fernando Valley"/>
+                        <CardMedia>
+                            <img src="/images/harvest_fb.png" />
+                        </CardMedia>
+                        <CardText>
+                            We’re a community of believers with a big heart. <br />
+                            We look forward to serving you in the Love of Christ, <br />
+                            and trust that you’ll find this to be a place of Hope and Faith <br />
+                        </CardText>
+                        <CardActions>
+                            <FlatButton label="Services" href="http://efcsfv.org/harvest/services" target="_blank"/>
+                            <FlatButton label="Facebook" href="https://facebook.com/harvestsfv" target="_blank"/>
+                            <FlatButton label="What We Believe" href="http://efcsfv.org/harvest/statement-of-faith/" target="_blank"/>
+                        </CardActions>
+                    </Card>
+                </Paper>
+            </Tab>
+
+            <Tab label="Schedule">
+
+            </Tab>
+
+            <Tab label="Registration">
+
+            </Tab>
+        </Tabs>
     }
 }
 
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,17 +1,50 @@
 import React from 'react'
 import style from '../css/home.css'
-import {Card, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 import Day1 from './daycards/Day1.jsx'
 import Day2 from './daycards/Day2.jsx'
 import Day3 from './daycards/Day3.jsx'
 import Day4 from './daycards/Day4.jsx'
 import Day5 from './daycards/Day5.jsx'
+import FlatButton from 'material-ui/FlatButton'
+import {Card,CardActions,  CardMedia, CardText, CardTitle} from 'material-ui/Card'
 
 class Home extends React.Component {
     render(){
         return <div>
+
             <div className={style.container}>
                 <img src="images/big-hero.png" className={style.heroImage}/>
+
+                <div className={style.registerNow}>
+                    <Card>
+                        <CardTitle title="VBS 2017 - Maker Fun Factory!" subtitle="presented by Harvest San Fernando Valley" />
+                        <CardText>
+                            <strong>What is VBS? </strong><br />
+                            Vacation Bible School or VBS is a wonderful opportunity for your child to learn more about
+                            God through music, dancing, games, science and taking a daily biblical adventure. A daily
+                            bible point will be reinforced throughout the day's activities.
+                            <br /><br />
+
+                            <strong>What are the dates?</strong><br />
+                            June 19th-23rd <br />
+                            9 AM - 12 PM (half day) <br />
+                            9 AM - 3 PM (full day)
+
+                            <br /><br />
+
+                            <strong>Where is it being held?</strong><br />
+                            Evangelical Formosan Church of San Fernando Valley <br />
+                            7702 Tampa Ave., <br />
+                            Reseda, CA 91335 <br />
+                            <a href="https://goo.gl/maps/jdHLArtxzgK2" target="_blank">Directions</a>
+                        </CardText>
+                        <CardActions>
+                            <FlatButton label="Learn More" href="/about" primary={true}/>
+                            <FlatButton label="Register Now" href="/register" secondary={true}/>
+                        </CardActions>
+                    </Card>
+
+                </div>
             </div>
 
             <div className={style.container}>
